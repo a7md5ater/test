18da9f94747aacb8763602fae78f7fab90689d5a
a7md5ater <ahmedkhater142000@gmail.com>
2024-04-27 21:14:33 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:test/app/app.dart';
 import 'package:test/app/injector.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   setupGetIt();
   runApp(const MyApp());
 }
